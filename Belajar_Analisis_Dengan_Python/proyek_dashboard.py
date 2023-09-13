034f2c43d49a2b49062f56890c16e944597b313b
darman1725 <darmansaragih1701@gmail.com>
2023-09-13 15:34:31 +0700

    Melengkapi module yang kurang lengkap

---
+++ Belajar_Analisis_Dengan_Python/proyek_dashboard.py
@@ -0,0 +1,7 @@
+# Review mandiri adalah proses Anda memastikan semua checklist submission sudah terpenuhi. Pastikan Anda mencentang semua checklist yang tersedia.
+# 1. Memastikan seluruh ketentuan pengiriman submission
+# 2. Menggunakan salah satu dari dataset yang telah disediakan
+# 3. Melakukan seluruh proses analisis data
+# 4. Proses analisis dibuat dalam notebook yang rapi
+# 5. Membuat dashboard sederhana menggunakan streamlit
+
